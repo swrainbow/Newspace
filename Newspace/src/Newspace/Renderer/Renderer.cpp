86a7c0f27247577ef497ac07427a62b93c668748
swrainbow <534965993@qq.com>
2023-06-02 00:30:18 +0800

    abstract shader

--- Newspace/src/Newspace/Renderer/Renderer.cpp
+++ Newspace/src/Newspace/Renderer/Renderer.cpp
@@ -1,5 +1,6 @@
 #include "nspacepch.h"
 #include "Renderer.h"
+#include "Platform/OpenGL/OpenGLShader.h"
 
 namespace Newspace {
 
@@ -18,8 +19,8 @@ namespace Newspace {
 	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
-		shader->UploadUniformMat4("u_Transform", transform);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,11 @@
 #include <Newspace.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public Newspace::Layer
 {
@@ -88,9 +91,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new Newspace::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Newspace::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -107,28 +110,30 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			
+			uniform vec3 u_Color;
 
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Newspace::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Newspace::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Newspace::Timestep ts) override
 	{
 		if (Newspace::Input::IsKeyPressed(HZ_KEY_LEFT))
-			m_CameraPosition.x += m_CameraMoveSpeed * ts;
-		else if (Newspace::Input::IsKeyPressed(HZ_KEY_RIGHT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
+		else if (Newspace::Input::IsKeyPressed(HZ_KEY_RIGHT))
+			m_CameraPosition.x += m_CameraMoveSpeed * ts;
 
 		if (Newspace::Input::IsKeyPressed(HZ_KEY_UP))
 			m_CameraPosition.y += m_CameraMoveSpeed * ts;
@@ -150,13 +155,16 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Newspace::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Newspace::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Newspace::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Newspace::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -167,7 +175,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Newspace::Event& event) override
@@ -177,7 +187,7 @@ private:
 	std::shared_ptr<Newspace::Shader> m_Shader;
 	std::shared_ptr<Newspace::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Newspace::Shader> m_BlueShader;
+	std::shared_ptr<Newspace::Shader> m_FlatColorShader;
 	std::shared_ptr<Newspace::VertexArray> m_SquareVA;
 
 	Newspace::OrthographicCamera m_Camera;
@@ -186,6 +196,8 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Newspace::Application
