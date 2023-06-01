4a2d693c4f21b5bf737880da17a4ea220d0317b8
swrainbow <534965993@qq.com>
2023-06-02 01:22:23 +0800

    add ref

--- Newspace/src/Newspace/Core.h
+++ Newspace/src/Newspace/Core.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <memory>
 #ifdef NSPACE_PLATFORM_WINDOWS
 #if NSPACE_DYNAMIC_LINK
 #ifdef NSPACE_BUILD_DLL
@@ -29,3 +29,13 @@
 #define BIT(x) (1 << x)
 
 #define NSPACE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace Newspace {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+}
