68bd09a0df3c6cd48ac6513197e55642307dcaea
swrainbow <534965993@qq.com>
2023-06-02 17:18:52 +0800

    shaders read from file

--- Newspace/src/Newspace/Renderer/Shader.cpp
+++ Newspace/src/Newspace/Renderer/Shader.cpp
@@ -6,6 +6,18 @@
 
 namespace Newspace {
 
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    NSPACE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return new OpenGLShader(filepath);
+		}
+
+		NSPACE_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
