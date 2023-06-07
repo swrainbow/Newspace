7335f71dcf82385c9e6dcb3a9c6d3ac961460e59
swrainbow <534965993@qq.com>
2023-06-07 13:45:51 +0800

    add batch render

--- Newspace/src/Newspace/Debug/Instrumentor.h
+++ Newspace/src/Newspace/Debug/Instrumentor.h
@@ -159,7 +159,7 @@ namespace Newspace {
 
 }
 
-#define NSPACE_PROFILE 1
+#define NSPACE_PROFILE 0
 #if NSPACE_PROFILE
 // Resolve which function signature macro will be used. Note that this only
 // is resolved when the (pre)compiler starts, so the syntax highlighting
