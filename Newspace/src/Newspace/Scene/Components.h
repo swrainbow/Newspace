7851d8e9945b7c6270c55bf7b21a0213b6013372
swrainbow <534965993@qq.com>
2023-06-13 16:58:40 +0800

    transform component

--- Newspace/src/Newspace/Scene/Components.h
+++ Newspace/src/Newspace/Scene/Components.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #include <glm/glm.hpp>
-#include "Newspace/Renderer/Camera.h"
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "SceneCamera.h"
 #include "ScriptableEntity.h"
 
-
 namespace Newspace {
-	
 
 	struct TagComponent
 	{
@@ -21,15 +20,25 @@ namespace Newspace {
 
 	struct TransformComponent
 	{
-		glm::mat4 Transform{ 1.0 };
+		glm::vec3 Translation = { 0.0f, 0.0f, 0.0f };
+		glm::vec3 Rotation = { 0.0f, 0.0f, 0.0f };
+		glm::vec3 Scale = { 1.0f, 1.0f, 1.0f };
 
 		TransformComponent() = default;
 		TransformComponent(const TransformComponent&) = default;
-		TransformComponent(const glm::mat4& transform) : Transform(transform)
-		{}
+		TransformComponent(const glm::vec3& translation)
+			: Translation(translation) {}
 
-		operator glm::mat4& () { return Transform; }
-		operator const glm::mat4& () const { return Transform; }
+		glm::mat4 GetTransform() const
+		{
+			glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), Rotation.x, { 1, 0, 0 })
+				* glm::rotate(glm::mat4(1.0f), Rotation.y, { 0, 1, 0 })
+				* glm::rotate(glm::mat4(1.0f), Rotation.z, { 0, 0, 1 });
+
+			return glm::translate(glm::mat4(1.0f), Translation)
+				* rotation
+				* glm::scale(glm::mat4(1.0f), Scale);
+		}
 	};
 
 	struct SpriteRendererComponent
@@ -40,12 +49,11 @@ namespace Newspace {
 		SpriteRendererComponent(const SpriteRendererComponent&) = default;
 		SpriteRendererComponent(const glm::vec4& color)
 			: Color(color) {}
-
 	};
 
 	struct CameraComponent
 	{
-		SceneCamera  Camera;
+		SceneCamera Camera;
 		bool Primary = true; // TODO: think about moving to Scene
 		bool FixedAspectRatio = false;
 
@@ -57,9 +65,6 @@ namespace Newspace {
 	{
 		ScriptableEntity* Instance = nullptr;
 
-		std::function<void()> InstantiateFunction;
-		std::function<void()> DestroyInstanceFunction;
-
 		ScriptableEntity* (*InstantiateScript)();
 		void (*DestroyScript)(NativeScriptComponent*);
 
@@ -70,4 +75,5 @@ namespace Newspace {
 			DestroyScript = [](NativeScriptComponent* nsc) { delete nsc->Instance; nsc->Instance = nullptr; };
 		}
 	};
+
 }
